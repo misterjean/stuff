e911479459f7afced9ddf3ea374cf1e5fea7c89b
Jean-Elie <jeanelie.jg@gmail.com>
2016-10-09 14:04:32 -0400

    ...

--- ip-notify/ip-notify.sh
+++ ip-notify/ip-notify.sh
@@ -8,7 +8,7 @@ then
         echo "Oh no! $CURIP has been modified in the last hour."
         # Send an email
 	echo "Don't worry, I'm sending you an email with your new IP.."
-        echo "Jean, your new IP is $NEWIP" | mail -s "ATTENTION: Your IP has changed" jeanelie.jg@gmail.com
+        echo "Jean, your new IP is $NEWIP" | mail -s "ATTENTION: Your IP has changed" <ENTER YOUR EMAIL HERE>
 	echo "Updating your IP on record on our end.."
 	echo "$NEWIP" > ~/Scripts/myip.txt
 	echo "All done!"
